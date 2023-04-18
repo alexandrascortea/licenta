564144be5fe5f9f5de6c35c79ab9b5e0a260a213
Alexandra <alexandra.scortea@zegasoftware.com>
2023-04-18 20:20:42 +0300

    Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import logo from '../assets/img/private-chalet.png';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='bg-primary'>
+      <div className='container mx-auto text-white flex justify-between items-center'>
+        {/* logo */}
+        <a href='/'>
+          <img src={logo} alt='' />
+        </a>
+        <div className='text-center'>
+          Copyright &copy; 2023. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
